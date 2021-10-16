9021372c9e99bb53da9795a5d172907509852b58
Devon Hockley <devon@hockley.ca>
2021-10-15 19:35:19 -0600

    added parsing code

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,3 +1,18 @@
+use flate2::read::GzDecoder;
+use std::fs::File;
+use std::io::Read;
+use tar::{Archive, Entry};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./datasets/test.dat.gz").unwrap();
+    let mut decompressed = GzDecoder::new(file);
+
+
+
+    let mut string = String::new();
+    decompressed.read_to_string(&mut string).unwrap();
+    println!("Dat file contents: \n {}",string)
+
+
+
 }
